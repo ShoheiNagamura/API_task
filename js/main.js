3089d00a5fe81f45fe7a9f955a9f51c98e3ae104
shouhei <shohei.nagamura@gmail.com>
2022-11-15 00:35:12 +0900

    SNSなどのアイコンを追加

--- js/main.js
+++ js/main.js
@@ -18,32 +18,33 @@ $("button").on("click", function () {
         url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1037138608625946581&categoryId=11-74";
     }
 
-    // // 楽天レシピランキングAPIへリクエスト
-    // $.getJSON(url, (data) => {
-    //     const recipes = data.result;
-    //     updateText(recipes);
-    // });
+    // 楽天レシピランキングAPIへリクエスト
+    $.getJSON(url, (data) => {
+        const recipes = data.result;
+        updateText(recipes);
+    });
 
-    // const updateText = (data) => {
-    //     console.log(data.length);
-    //     for (let i = 0; i < data.length; i++) {
-    //         const insertHtml = `
-    //         <li class="recipeItem">
-    //             <p class="rank">${data[i].rank}位</p>
-    //             <a href="${data[i].recipeUrl}" target="_blank">
-    //                 <img src="${data[i].foodImageUrl}" alt="${data[i].recipeTitle} 画像">
-    //             </a>
-    //             <h2 class="recipeTitle">${data[i].recipeTitle}</h2>
-    //             <p class="recipeDescription">${data[i].recipeDescription}</p>
-    //             <p class="material"><材料>${data[i].recipeMaterial}</p>
-    //             <p class="indication"><調理時間>${data[i].recipeIndication}</p>
-    //             <p class="cost"><費用の目安>${data[i].recipeCost}</p>
-    //         </li>
-    //     `;
-    //         console.log(insertHtml);
-    //         $('#recipe_list').append(insertHtml);
-    //     }
-    // }
+    const updateText = (data) => {
+        console.log(data.length);
+        for (let i = 0; i < data.length; i++) {
+            const insertHtml = `
+            <li class="recipeItem">
+                <p class="fish-tag">さかな料理レシピ</p>
+                <p class="rank">${data[i].rank}位</p>
+                <a href="${data[i].recipeUrl}" target="_blank">
+                    <img src="${data[i].foodImageUrl}" alt="${data[i].recipeTitle} 画像">
+                </a>
+                <h2 class="recipeTitle">${data[i].recipeTitle}</h2>
+                <p class="recipeDescription">${data[i].recipeDescription}</p>
+                <p class="material"><材料><br>${data[i].recipeMaterial}</p>
+                <p class="indication"><調理時間>${data[i].recipeIndication}</p>
+                <p class="cost"><費用の目安>${data[i].recipeCost}</p>
+            </li>
+        `;
+            console.log(insertHtml);
+            $('#recipe_list').append(insertHtml);
+        }
+    }
 })
 
 $(".reset").on("click", function () {
@@ -52,13 +53,64 @@ $(".reset").on("click", function () {
 })
 
 
+// キーダウン
+$("button").on("keydown", function (e) {
+    //既存のレシピ表示を削除
+    if (e.keyCode === 13) {
+        $(".recipeItem").remove();
+
+        let y = $("#fish-name").val();
+
+        let url = "";
+        if (y === "サーモン" || y === "サケ" || y === "鮭" || y === "さけ") {
+            url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1037138608625946581&categoryId=11-70";
+        } else if (y === "いわし" || y === "イワシ" || y === "鰯") {
+            url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1037138608625946581&categoryId=11-71";
+        } else if (y === "さば" || y === "サバ" || y === "鯖") {
+            url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1037138608625946581&categoryId=11-72";
+        } else if (y === "あじ" || y === "アジ" || y === "鯵") {
+            url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1037138608625946581&categoryId=11-73";
+        } else if (y === "ぶり" || y === "ブリ" || y === "鰤") {
+            url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1037138608625946581&categoryId=11-74";
+        }
+
+        // 楽天レシピランキングAPIへリクエスト
+        $.getJSON(url, (data) => {
+            const recipes = data.result;
+            updateText(recipes);
+        });
+
+        const updateText = (data) => {
+            console.log(data.length);
+            for (let i = 0; i < data.length; i++) {
+                const insertHtml = `
+            <li class="recipeItem">
+                <p class="fish-tag">さかな料理レシピ</p>
+                <p class="rank">${data[i].rank}位</p>
+                <a href="${data[i].recipeUrl}" target="_blank">
+                    <img src="${data[i].foodImageUrl}" alt="${data[i].recipeTitle} 画像">
+                </a>
+                <h2 class="recipeTitle">${data[i].recipeTitle}</h2>
+                <p class="recipeDescription">${data[i].recipeDescription}</p>
+                <p class="material"><材料><br>${data[i].recipeMaterial}</p>
+                <p class="indication"><調理時間>${data[i].recipeIndication}</p>
+                <p class="cost"><費用の目安>${data[i].recipeCost}</p>
+                
+            </li>
+        `;
+                console.log(insertHtml);
+                $('#recipe_list').append(insertHtml);
+            }
+        }
+    }
+})
 
 
-ホットペッパーからお店情報を取得
-$("fuga").on("click", function () {
 
 
-
+// ホットペッパーからお店情報を取得
+$(".restaurant").on("click", function () {
+    $("#recipe_list").hide();
     //飲食店をAPIで検索
     // 現在の位置を取得
     function showPosition(position) {
@@ -70,7 +122,7 @@ $("fuga").on("click", function () {
         // ホットペッパーAPIキーセット
         // let API_KEY = '38f4bc42aba3b5a1';
 
-        let URL = '//webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=38f4bc42aba3b5a1'
+        let URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=38f4bc42aba3b5a1'
 
         // $.ajax({
         //     url: 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=38f4bc42aba3b5a1',
