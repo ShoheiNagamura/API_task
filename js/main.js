d6c35b974920c109c64555c5f2505832d8f6d814
shouhei <shohei.nagamura@gmail.com>
2022-11-14 23:08:14 +0900

    ヘッダーにボタンを追加

--- js/main.js
+++ js/main.js
@@ -53,78 +53,85 @@ $(".reset").on("click", function () {
 
 
 
-//飲食店をAPIで検索
-// 現在の位置を取得
-function showPosition(position) {
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
 
-    // ホットペッパーAPIキーセット
-    // let API_KEY = '38f4bc42aba3b5a1';
+ホットペッパーからお店情報を取得
+$("fuga").on("click", function () {
 
-    let URL = '//webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=38f4bc42aba3b5a1'
 
-    // $.ajax({
-    //     url: 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=38f4bc42aba3b5a1',
-    //     type: 'GET',
-    //     dataType: 'jsonp',
-    //     jsonpCallback: 'callback'
-    // }).done(function (data) {
-    //     var dummy = data; // 成功時 この処理はダミーなので変更してください
-    // }).fail(function (data) {
-    //     var dummy = data; // 失敗時
-    // });
-    // + '&lat=33.590543&lng=130.420096&range=2&order=1&format=json'
-    // http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=[APIキー]&lat=33.590543&lng=130.420096&range=2&order=1&format=json
 
-    $.getJSON(URL, (data) => {
-        const recipes = data.result;
-        console.log(recipes);
-        // updateText(recipes);
-    });
+    //飲食店をAPIで検索
+    // 現在の位置を取得
+    function showPosition(position) {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
 
-    // const updateText = (data) => {
-    //     console.log(data.length);
-    //     for (let i = 0; i < data.length; i++) {
-    //         const insertHtml = `
-    //         <li class="recipeItem">
-    //             <p class="rank">${data[i].rank}位</p>
-    //             <a href="${data[i].recipeUrl}" target="_blank">
-    //                 <img src="${data[i].foodImageUrl}" alt="${data[i].recipeTitle} 画像">
-    //             </a>
-    //             <h2 class="recipeTitle">${data[i].recipeTitle}</h2>
-    //             <p class="recipeDescription">${data[i].recipeDescription}</p>
-    //             <p class="material"><材料>${data[i].recipeMaterial}</p>
-    //             <p class="indication"><調理時間>${data[i].recipeIndication}</p>
-    //             <p class="cost"><費用の目安>${data[i].recipeCost}</p>
-    //         </li>
-    //     `;
-    //         console.log(insertHtml);
-    //         $('#recipe_list').append(insertHtml);
-    //     }
-    // }
-}
+        // ホットペッパーAPIキーセット
+        // let API_KEY = '38f4bc42aba3b5a1';
 
-function showError(error) {
-    console.log(error);
-    const errorMessage = [
-        "位置情報が許可されていません",
-        "現在位置を特定できません"
-    ];
-    alert(`error:${errorMessage[error.code - 1]}`);
-}
+        let URL = '//webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=38f4bc42aba3b5a1'
 
-const option = {
-    enableHighAccuracy: true,
-    maximumAge: 10000,
-    timeout: 10000,
-};
+        // $.ajax({
+        //     url: 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=38f4bc42aba3b5a1',
+        //     type: 'GET',
+        //     dataType: 'jsonp',
+        //     jsonpCallback: 'callback'
+        // }).done(function (data) {
+        //     var dummy = data; // 成功時 この処理はダミーなので変更してください
+        // }).fail(function (data) {
+        //     var dummy = data; // 失敗時
+        // });
+        // + '&lat=33.590543&lng=130.420096&range=2&order=1&format=json'
+        // http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=[APIキー]&lat=33.590543&lng=130.420096&range=2&order=1&format=json
 
-// geolocation.html
+        $.getJSON(URL, (data) => {
+            const recipes = data.result;
+            console.log(recipes);
+            // updateText(recipes);
+        });
 
-navigator.geolocation.getCurrentPosition(showPosition, showError, option);
+        // const updateText = (data) => {
+        //     console.log(data.length);
+        //     for (let i = 0; i < data.length; i++) {
+        //         const insertHtml = `
+        //         <li class="recipeItem">
+        //             <p class="rank">${data[i].rank}位</p>
+        //             <a href="${data[i].recipeUrl}" target="_blank">
+        //                 <img src="${data[i].foodImageUrl}" alt="${data[i].recipeTitle} 画像">
+        //             </a>
+        //             <h2 class="recipeTitle">${data[i].recipeTitle}</h2>
+        //             <p class="recipeDescription">${data[i].recipeDescription}</p>
+        //             <p class="material"><材料>${data[i].recipeMaterial}</p>
+        //             <p class="indication"><調理時間>${data[i].recipeIndication}</p>
+        //             <p class="cost"><費用の目安>${data[i].recipeCost}</p>
+        //         </li>
+        //     `;
+        //         console.log(insertHtml);
+        //         $('#recipe_list').append(insertHtml);
+        //     }
+        // }
+    }
 
+    function showError(error) {
+        console.log(error);
+        const errorMessage = [
+            "位置情報が許可されていません",
+            "現在位置を特定できません"
+        ];
+        alert(`error:${errorMessage[error.code - 1]}`);
+    }
+
+    const option = {
+        enableHighAccuracy: true,
+        maximumAge: 10000,
+        timeout: 10000,
+    };
+
+    // geolocation.html
+
+    navigator.geolocation.getCurrentPosition(showPosition, showError, option);
+
+})
 
 
